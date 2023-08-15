f5145a918294353670e5b595b20b5f4af0bdf3cf
ThirdPrince <18796017665@163.com>
2023-08-15 22:51:25 +0800

    update README.MD

--- src/kt/AsyncTask.kt
+++ src/kt/AsyncTask.kt
@@ -1,41 +1,44 @@
 package kt
 
-import kotlinx.coroutines.async
-import kotlinx.coroutines.coroutineScope
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.*
 import utils.log
 
 
-fun main() = runBlocking{
-    val userPortraitImageMap: MutableMap<Long, String> = mutableMapOf()
+suspend fun main() {
 
-    userPortraitImageMap[1]  = "123"
-    userPortraitImageMap[2] =  "456"
-    userPortraitImageMap[3] = "789"
-    userPortraitImageMap[4] = "012"
+    coroutineScope{
+        val userPortraitImageMap: MutableMap<Long, String> = mutableMapOf()
+
+        userPortraitImageMap[1]  = "123"
+        userPortraitImageMap[2] =  "456"
+        userPortraitImageMap[3] = "789"
+        userPortraitImageMap[4] = "012"
+        userPortraitImageMap[5] = "555"
+        userPortraitImageMap[6] = "7777"
+        userPortraitImageMap[8] = "88888"
 
 
-    if (userPortraitImageMap.isNotEmpty()) {
-        coroutineScope {
-            val deferredResults = userPortraitImageMap.map { (userId, fileId) ->
-                async {
-                    log("userId -->$userId 开始")
-                    val localPath = downloadPortraitPhoto(fileId)
-                    log("----$localPath")
-                    userId to localPath
+        if (userPortraitImageMap.isNotEmpty()) {
+            var count = userPortraitImageMap.size
+            coroutineScope {
+                val deferredResults = userPortraitImageMap.map { (userId, fileId) ->
+                    async {
+                        log("userId -->$userId 开始")
+                        val localPath = downloadPortraitPhoto(fileId)
+                        log("还剩下---${--count}")
+                        userId to localPath
+                    }
                 }
-            }
-            println(deferredResults::class.simpleName)
 
-            val resultMap = deferredResults.map { deferred ->
-                val (userId, localPath) = deferred.await()
-                log("userId -->$userId 结束")
-                userId to localPath
-            }.toMap()
-            log("全部 结束")
-            resultMap.forEach {
-                println("${it.key}"+":${it.value}")
+                val resultMap = deferredResults.map { deferred ->
+                    val (userId, localPath) = deferred.await()
+                    log("userId -->$userId 结束")
+                    userId to localPath
+                }.toMap()
+                log("全部 结束")
+                resultMap.forEach {
+                    println("${it.key}"+":${it.value}")
+                }
             }
         }
     }
